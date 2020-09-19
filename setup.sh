fdeea7717c2e553fa54611fc3501e3e980fbf795
Chris Wiederspan <chris@wiederspan.com>
2020-09-19 12:03:25 -0600

    Updated folder structure

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-alias k=kubectl
+#================
+# bash
+#================
+
+echo "Setting up your dotfiles..."
+
+ln -sf ./bash/.bashrc ~/
+ln -sf ./bash/.bash_aliases ~/
+# ln -sf $dotfiles_dir/bash/.bash_profile ~/
+# ln -sf $dotfiles_dir/bash/.inputrc ~/
