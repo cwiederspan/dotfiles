e4f63cce5fc55d2875f88a98b7ac8bddbf31c011
Chris Wiederspan <chris@wiederspan.com>
2020-09-20 14:28:50 -0600

    Updated bash file mappings

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 
 echo "Setting up your dotfiles..."
 
-ln -sf ./bash/.bashrc ~/
-ln -sf ./bash/.bash_aliases ~/
+ln -sf ~/dotfiles/bash/.bashrc ~/
+ln -sf ~/dotfiles/bash/.bash_aliases ~/
 # ln -sf $dotfiles_dir/bash/.bash_profile ~/
 # ln -sf $dotfiles_dir/bash/.inputrc ~/
