7038fbf8c59445d4e79b90237bca3a6f850ba1aa
Chris Wiederspan <chris@wiederspan.com>
2020-10-14 19:19:51 -0600

    Adding powerline fonts

--- setup.sh
+++ setup.sh
@@ -6,9 +6,16 @@
 
 echo "Setting up your dotfiles..."
 
+# Powerline Fonts (https://github.com/powerline/fonts)
+sudo apt-get install fonts-powerline
+
+# Bash mappings
+
 ln -sf ~/dotfiles/bash/.bashrc ~/
 ln -sf ~/dotfiles/bash/.bash_aliases ~/
 # ln -sf $dotfiles_dir/bash/.bash_profile ~/
 # ln -sf $dotfiles_dir/bash/.inputrc ~/
+
+# ZSH mappings
 
 ln -sf ~/dotfiles/zsh/.zshrc ~/
