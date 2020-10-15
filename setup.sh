fe520950d20d2be829683291bf2e5da5393b0d6f
Chris Wiederspan <chris@wiederspan.com>
2020-10-14 19:14:55 -0600

    Revamp of .zshrc

--- setup.sh
+++ setup.sh
@@ -11,10 +11,4 @@ ln -sf ~/dotfiles/bash/.bash_aliases ~/
 # ln -sf $dotfiles_dir/bash/.bash_profile ~/
 # ln -sf $dotfiles_dir/bash/.inputrc ~/
 
-# Powerline Fonts (https://github.com/powerline/fonts)
-#sudo apt-get install fonts-powerline
-
-# Powerline-go (https://github.com/justjanne/powerline-go)
-#sudo go get -u github.com/justjanne/powerline-go
-
-source ~/dotfiles/zsh/.zshrc
+ln -sf ~/dotfiles/zsh/.zshrc ~/
