ad6facf9724c8062a533566b18518b0329e20304
Chris Wiederspan <chris@wiederspan.com>
2020-10-28 16:33:30 -0600

    Renaming setup.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#================
+# bash
+#================
+
+echo "Setting up your dotfiles..."
+
+# Bash mappings
+ln -sf ~/dotfiles/bash/.bashrc ~/
+
+# ZSH mappings
+#ln -sf ~/dotfiles/zsh/.zshrc ~/
