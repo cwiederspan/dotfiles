eb34ed9e9f593cc62f60a74728be6e2f5053dfed
Chris Wiederspan <chris@wiederspan.com>
2020-10-28 16:47:00 -0600

    Updated to use only install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,27 @@
 
 echo "Setting up your dotfiles..."
 
+alias xxx=kubectl
+
+
+# Powerline-go (https://github.com/justjanne/powerline-go)
+go get -u github.com/justjanne/powerline-go
+
+function _update_ps1() {
+    PS1="$($GOPATH/bin/powerline-go -error $?)"
+}
+
+if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+    echo "I AM HERE!!!"
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+
+
 # Bash mappings
-ln -sf ~/dotfiles/bash/.bashrc ~/
+#ln -sf ~/dotfiles/bash/.bashrc ~/
 
 # ZSH mappings
 #ln -sf ~/dotfiles/zsh/.zshrc ~/
+
+
+alias k=kubectl
