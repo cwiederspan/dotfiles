75bd4ef580c50aecd6f5a2ffc4e96dd7947f4c0d
Chris Wiederspan <chris@wiederspan.com>
2020-10-29 10:18:55 -0600

    Udpated back to old working version

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "*** TESTING 1, TESTING 1, TESTING 1 ***"
-
-git clone https://github.com/bhilburn/powerlevel9k.git
-
-echo "*** TESTING 2, TESTING 2, TESTING 2 ***"
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Setting up your dotfiles..."
+
+# Bash mappings
+
+ln -sf ~/dotfiles/bash/.bashrc ~/
+# ln -sf ~/dotfiles/bash/.bash_aliases ~/
+# ln -sf $dotfiles_dir/bash/.bash_profile ~/
+# ln -sf $dotfiles_dir/bash/.inputrc ~/
