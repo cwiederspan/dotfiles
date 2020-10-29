1c08758ce7aaef220a1cacb1f2ed58cecfdc633a
Chris Wiederspan <chris@wiederspan.com>
2020-10-29 09:59:07 -0600

    testing

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/bhilburn/powerlevel9k.git
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "*** TESTING 1, TESTING 1, TESTING 1 ***"
+
+git clone https://github.com/bhilburn/powerlevel9k.git
+
+echo "*** TESTING 2, TESTING 2, TESTING 2 ***"
