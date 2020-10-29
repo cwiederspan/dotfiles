73d034b0d25de056a5a09a86a1f9b1949c13dbbe
Chris Wiederspan <chris@wiederspan.com>
2020-10-29 09:20:02 -0600

    Trying a git test

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+echo "*** TESTING, TESTING, TESTING ***";
+
+git clone https://github.com/bhilburn/powerlevel9k.git;
--- xxx_install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "*** TESTING, TESTING, TESTING ***";
