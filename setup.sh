586fb8bf78419af5f6423ee8758ca4ca974f5ca9
Chris Wiederspan <chris@wiederspan.com>
2021-07-02 17:28:20 -0600

    Updates setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Setting up your dotfiles..."
+
+echo WORKING DIR
+pwd
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo SCRIPT PATH
+echo $SCRIPTPATH
+
+sudo unlink ~/.bashrc
+ln -sv $SCRIPTPATH/.bashrc ~
+
+source ~/.bashrc
