d970678ce132ec321f2b61ab420607162ebca761
Chris Wiederspan <chris@wiederspan.com>
2021-07-02 17:14:56 -0600

    Reverting back to last change

--- setup.sh
+++ setup.sh
@@ -14,11 +14,9 @@ go get -u github.com/justjanne/powerline-go
 # Bash mappings
 
 # ln -sf ~/dotfiles/bash/.bash_profile ~/
-# ln -sf ~/dotfiles/bash/.bashrc ~/
+ln -sf ~/dotfiles/bash/.bashrc ~/
 # ln -sf ~/dotfiles/bash/.bash_aliases ~/
 # ln -sf $dotfiles_dir/bash/.bash_profile ~/
 # ln -sf $dotfiles_dir/bash/.inputrc ~/
-
-ln -sf bash/.bashrc ~/
 
 source ~/.bashrc
