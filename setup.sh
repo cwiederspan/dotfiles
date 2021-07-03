c0e94bb62e5248b13248d0fb0d4f34da2553c358
Chris Wiederspan <chris@wiederspan.com>
2021-07-02 18:11:06 -0600

    Updated files

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 echo "Setting up your dotfiles..."
 
+go get -u github.com/justjanne/powerline-go
+
+function _update_ps1() {
+    PS1="$($GOPATH/bin/powerline-go -error $?)"
+}
+
+if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+
 echo WORKING DIR
 pwd
 
